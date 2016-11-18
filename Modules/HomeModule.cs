e9406ed408a5171e3d0a885f310093f7b304eb09
Bryant Wang <bryant.m.wang@gmail.com>
2016-11-18 09:06:48 -0800

    Add Contact and Address objects with private properties and get and set methods.

---
+++ Modules/HomeModule.cs
@@ -0,0 +1,16 @@
+using Nancy;
+using AddressBook.Objects;
+using System.Collections.Generic;
+
+namespace AddressBook
+{
+  public class HomeModule : NancyModule
+  {
+    public HomeModule()
+    {
+      Get["/"] = _ => {
+        return View["index.cshtml"];
+      }
+    }
+  }
+}
---
+++ Objects/Contact.cs
@@ -0,0 +1,58 @@
+using System.Collections.Generic;
+
+namespace AddressBook.Objects
+{
+  public class Contact
+  {
+    private string _name;
+    private string _phone;
+    private Address _address;
+    private static List<Contact> _contacts = new List<Contact> {};
+
+    public Contact(string contactName, string contactPhone)
+    {
+      _name = contactName;
+      _phone = contactPhone;
+    }
+
+    public void SetName(string contactName)
+    {
+      _name = contactName;
+    }
+
+    public string GetName()
+    {
+      return _name;
+    }
+
+    public void SetPhone(string contactPhone)
+    {
+      _phone = contactPhone;
+    }
+
+    public string GetPhone()
+    {
+      return _phone;
+    }
+
+    public void SetAddress(Address contactAddress)
+    {
+      _address = contactAddress;
+    }
+
+    public Address GetAddress()
+    {
+      return _address;
+    }
+
+    public static void Add()
+    {
+      _contacts.Add(this);
+    }
+
+    public List<Contact> GetAll()
+    {
+      return _contacts;
+    }
+  }
+}
